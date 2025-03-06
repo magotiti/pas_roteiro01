8e41882359836a91a918fc293b19fd0558d4b0cc
Thiago Cardoso <127211817+magotiti@users.noreply.github.com>
2025-03-06 00:49:25 +0000

    projeto1a

--- App.java
+++
@@ -1,7 +0,0 @@
-public class App {
- public static void main(String[] args) throws Exception {
- System.out.println("\nHello!");
- String version = System.getProperty("java.version");
- System.out.println("Running Java Version "+version+"\n");
- }
-}
